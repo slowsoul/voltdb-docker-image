43b624eec8528c0b8b23da88ae01b1f242e4f9c4
Rui Shen <rui@voltdb.com>
2015-07-28 17:07:58 -0400

    Add some sleep before exec

--- run.sh
+++ run.sh
@@ -14,5 +14,7 @@ docker run -d -P --name $2 $1 \
 sh -c "groupadd $GROUP;useradd $USER -m -g $GROUP;chown -R $USER:$GROUP /opt /tmp /home/$USER;
 echo 'export PATH=\$PATH:/opt/voltdb/bin;cd /home/$USER;$CMD' | su - $USER;"
 
+sleep 5
+
 docker exec -i -t -u $USER $2 bash
 
